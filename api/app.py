2c8a5670708f61ee348ca7ed5ecafc9f0170514a
Loretarojas <168664585+Loretarojas@users.noreply.github.com>
2024-05-28 15:04:58 -0300

    otra api para probar

--- api/app.py
+++ api/app.py
@@ -1,15 +1,10 @@
-from flask import Flask, send_from_directory
-import os
-import random
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
-@app.route('/random-photo', methods=['GET'])
-def get_random_photo():
-    image_folder = os.path.join(app.static_folder, 'images')
-    image_files = [f for f in os.listdir(image_folder) if f.endswith('.jpg')]
-    random_image = random.choice(image_files)
-    return send_from_directory(image_folder, random_image)
+@app.route('/get_image')
+def get_image():
+    return redirect("https://picsum.photos/200/300")
 
 if __name__ == '__main__':
     app.run(debug=True)
