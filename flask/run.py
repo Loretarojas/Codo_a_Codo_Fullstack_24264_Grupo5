72baeb06ef1adff5e7f8d4e0eded05a18a0801b4
Carito Morisetti <caromorisetti>
2024-07-07 22:27:31 +0200

    dependencias y conexion base de datos

--- flask/run.py
+++ flask/run.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from app.views import *
+from app.database import test_connection, init_app
 
 app = Flask(__name__)
 
 # Rutas de Api-Rest 
 
 app.route('/', methods=['GET'])(index)
+
+# Conexión a BDD
+init_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
