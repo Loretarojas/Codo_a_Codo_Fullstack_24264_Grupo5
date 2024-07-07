9d8d4696f6a70b7e470b10b991c55c8af177e1d6
Carito Morisetti <caromorisetti>
2024-07-07 20:32:27 +0200

    cambios

--- flask/run.py
+++ flask/run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Rutas de Api-Rest 
 
-app.route(' /', methods=['GET'])(index)
+app.route('/', methods=['GET'])(index)
 
 if __name__ == '__main__':
     app.run(debug=True)
