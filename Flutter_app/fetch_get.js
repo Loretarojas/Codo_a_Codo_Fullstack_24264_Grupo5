3d59882d08de7e737c7f330e530b4548cfde4b11
Carito Morisetti <caromorisetti>
2024-07-14 23:05:57 +0200

    cambios

--- Flutter_app/fetch_get.js
+++ Flutter_app/fetch_get.js
@@ -1,30 +1,32 @@
 let container = document.querySelector(".container");
 
+let mariposaTemplate = document.querySelector(".form-carga");
+
+
+
 
 fetchData(
-    "http://127.0.0.1:5000/mariposa/",
+    "http://localhost:5000/mariposa/",
     "GET",
     (data) => {
         console.log(data);
 
         let mariposas = [];
 
-        for(const mariposa of data){
+        for(const mariposa of data) {
             console.log(mariposa);
+          
+            let newMariposa = mariposaCompleted.cloneNode(true);
 
-        let newMariposa = document.createElement("div");
-
-        newMariposa.querySelector(".nombre").innerHTML = mariposa.nombre;
-        newMariposa.querySelector(".especie").innerHTML = mariposa.especie;
-        newMariposa.querySelector(".familia").innerHTML = mariposa.familia;
-        newMariposa.querySelector(".nombrecientifico").innerHTML = mariposa.nombrecientifico;
-        newMariposa.querySelector(".informacion").innerHTML = mariposa.informacion;
-        newMariposa.querySelector(".pais").id = `pais-${mariposa.id}`;
-        newMariposa.querySelector("#migratoria").id = `migratoria-${mariposa.id}`;
-
-
-        mariposas.push(newMariposa);
+            newMariposa.querySelector(".nombre").innerHTML = mariposa.nombre;
+            newMariposa.querySelector(".especie").innerHTML = mariposa.especie;
+            newMariposa.querySelector(".familia").innerHTML = mariposa.familia;
+            newMariposa.querySelector(".nombreCientifico").innerHTML = mariposa.nombreCientifico;
+            newMariposa.querySelector(".pais").value = mariposa.pais;
+            newMariposa.querySelector(".peligroExtincion").checkbox = mariposa.peligroExtincion;
+            newMariposa.querySelector(".migratoria").checkbox = mariposa.migratoria;
+          
+            mariposas.push(newMariposa);
         }
-
     }
- );
+);
--- Flutter_app/fetch_post.js
+++ Flutter_app/fetch_post.js
@@ -1,19 +1,25 @@
-let submitButton = document.querySelector("#butterfly-search-button");
+let submitButton = document.querySelector(".butterfly-search-button");
 
 submitButton.addEventListener("click", ()=>{
     let data_post = {
-        'nombre': document.querySelector("#nombre").value,
-        'asunto': document.querySelector("#asunto").value
+        'nombre': document.querySelector(".nombre").value,
+        'especie': document.querySelector(".especie").value,
+        'familia': document.querySelector(".familia").value,
+        'nombreCientifico': document.querySelector(".nombreCientifico").value,
+        'pais': document.querySelector(".pais").value,
+        'peligroExtincion': document.querySelector("#peligroExtincion").checked,
+        'migratoria': document.querySelector("#migratoria").checked
+       
     }
     
     fetchData(
         "http://localhost:5000/mariposa/create/",
         "POST",
         (data) => {
-            document.querySelector("#formulario").reset();
-            window.location.replace("../index.html");
+            document.querySelector(".form-carga").reset();
+            window.location.replace("./pages/cargabutterflies.html");
         },
         data_post
     );
-    }
-);
+    
+});
